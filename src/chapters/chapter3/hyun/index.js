1b602b34dc3852c6ce1fe12da43328a29f15a2f0
yanione <yanione@linecorp.com>
2020-07-09 15:21:15 +0900

    Add jeon path for each chapters

--- src/chapters/chapter3/hyun/index.js
+++ src/chapters/chapter3/hyun/index.js
@@ -30,4 +30,4 @@
 // import '@chapters/chapter3/hyun/never';
 
 // example 11
-import '@chapters/chapter3/hyun/throwError';
+// import '@chapters/chapter3/hyun/throwError';
--- src/chapters/index.js
+++ src/chapters/index.js
@@ -1,4 +1,4 @@
-import chapter1 from '@chapters/chapter1';
-import chapter2 from '@chapters/chapter2';
+import jeonChapter1 from '@chapters/chapter1/jeon';
+import jeonChapter2 from '@chapters/chapter2/jeon';
 
 import hyunChapter3 from '@chapters/chapter3/hyun';
